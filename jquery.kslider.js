c95621b9b4980d33bad9e63c3811ab05c051bef1
Стас <stan@irina.(none)>
2014-10-30 19:30:58 +0700

    add styles

--- jquery.kslider.js
+++ jquery.kslider.js
@@ -1,5 +1,9 @@
 ;(function( $ ) {
 	$.fn.kslider = function() {
+		// инициаизируем структуру и добавляем стилей
+		this.addClass("kslider");
+		$("<div/>").addClass("kslider-element").appendTo(this);
+
 		return this;
 	};
 }( jQuery ));
